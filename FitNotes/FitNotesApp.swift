dfbd1c5fb5ddbc8bd1ab96fdb8d5f1e2dba5f79c
Sergey Meshcheryakov <vazko163@gmail.com>
2026-05-23 12:14:41 +0400

    Initial Commit

---
+++ FitNotes/FitNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNotesApp.swift
+//  FitNotes
+//
+//  Created by Sergey Meshcheryakov on 23.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
